c297008f744784b69e6cc987410a5ae7a1b50664
Pavel Kuzmin <Virus191288@gmail.com>
2025-07-03 14:16:09 +0500

    Update switchshuttle.rb

--- Casks/switchshuttle.rb
+++ Casks/switchshuttle.rb
@@ -1,13 +1,8 @@
 cask "switchshuttle" do
   version "1.5.4"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/s00d/switchshuttle/releases/download/app-v#{version}/switch-shuttle_#{version}_x64.dmg"
-    sha256 "b29fc9fabd048b5209bda7c1887fdc2e8d07db5716925f143ca58477d1d22223"
-  else
-    url "https://github.com/s00d/switchshuttle/releases/download/app-v#{version}/switch-shuttle_#{version}_aarch64.dmg"
-    sha256 "f30fb5158e1a8cc7d2b310218e3f4a6fdc230848832a985f3b9f0776ea371cb8"
-  end
+  url "https://github.com/s00d/switchshuttle/releases/download/app-v#{version}/switch-shuttle_#{version}_universal.dmg"
+  sha256 "b29fc9fabd048b5209bda7c1887fdc2e8d07db5716925f143ca58477d1d22223"
 
   name "SwitchShuttle"
   desc "Cross-platform terminal command manager with global hotkeys - organize, customize, and quickly access your most-used terminal operations with a sleek interface"
@@ -28,4 +23,4 @@ cask "switchshuttle" do
   #   "~/Library/Preferences/com.SwitchShuttle.app.plist",
   #   "~/Library/Saved Application State/com.SwitchShuttle.app.savedState"
   # ]
-end 
+end
